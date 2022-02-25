563ab134072b524b22a3392666d3836811e08f08
SauravKumar <krsaurav.097@gmail.com>
2022-02-26 00:21:57 +0530

    ProjectStrecture

---
+++ src/main/java/com/starbanking/StarBankingandInsuranceApplication.java
@@ -0,0 +1,13 @@
+package com.starbanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarBankingandInsuranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarBankingandInsuranceApplication.class, args);
+	}
+
+}
